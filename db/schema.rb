4c345c86e650984b03286fa16063baa2598c70dc
Harsha <h.c.shinde@gmail.com>
2015-02-12 16:57:47 +0530

    migration for student

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212111744) do
+ActiveRecord::Schema.define(version: 20150212112634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "active"
+  end
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -51,6 +60,12 @@ ActiveRecord::Schema.define(version: 20150212111744) do
     t.datetime "updated_at"
     t.datetime "inserted_at"
     t.string   "title"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
